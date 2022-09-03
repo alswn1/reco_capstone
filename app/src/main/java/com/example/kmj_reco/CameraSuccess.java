9970f8c2cf1db6acfaa53da6d271cd2f83616701
KMJ <alswn5790@naver.com>
2022-09-03 19:22:44 +0900

    ai success & success layout finish

---
+++ app/src/main/java/com/example/kmj_reco/CameraSuccess.java
@@ -0,0 +1,26 @@
+package com.example.kmj_reco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class CameraSuccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_success);
+
+        Button backHome = findViewById(R.id.backHome);
+        backHome.setOnClickListener(view -> {
+            Intent i = new Intent(CameraSuccess.this, MainActivity.class);
+            startActivity(i);
+        });
+    }
+}
